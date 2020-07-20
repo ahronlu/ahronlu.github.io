5342a12b1c866d960bde203d49f3a043f7c26a02
Aharon Lubitsch <59685123+koftov@users.noreply.github.com>
2020-07-20 05:50:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i <= projects.length; i++) {
   container.innerHTML += `<div class="box" title='${projects[i].description}'>
-  <a target="_blank" href="https://koftovdevconnector.herokuapp.com/">
-        <h2>${
-          projects[i].name
-        }</h2></a><a target="_blank" href="https://koftovdevconnector.herokuapp.com/">
+  <a target="_blank" href=${projects[i].url}>
+        <h2>${projects[i].name}</h2></a><a target="_blank" href=${
+    projects[i].url
+  }>
         <img src=${getThumbnailUrl(projects[i].name)} alt="${
     projects[i].name
   }" /></a>
         <p class="tags">${tagsToChips(projects[i].tags)}</p>
         <div class="actions">
-        <a target="_blank" href="https://koftovdevconnector.herokuapp.com/">
+        <a target="_blank" href=${projects[i].url}>
         <i class="fas fa-eye"></i> View Live
         </a>
-        <a target="_blank" href="https://github.com/koftov/devconnector">
+        <a target="_blank" href=${projects[i].github}>
         <i class="fab fa-github"></i> Github
         </a></div>
 
