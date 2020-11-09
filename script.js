ae499788f976ce74b219de61bdb8e8be040c28c4
koftov <ahronlu@gmail.com>
2020-11-09 14:49:03 +0200

    for to forEach

--- script.js
+++ script.js
@@ -13,27 +13,23 @@ toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
 
-for (let i = 0; i < projects.length; i++) {
-  container.innerHTML += `<div class="${
-    projects[i].show ? "box" : "box hide"
-  }" title="${projects[i].description}">
-  <a target="_blank" href=${projects[i].url}>
-        <h2>${projects[i].name}</h2></a><a target="_blank" href=${
-    projects[i].url
-  }>
-        <img src=${getThumbnailUrl(projects[i].name)} alt="${
-    projects[i].name
-  }" /></a>
-        <p class="tags">${tagsToChips(projects[i].tags)}</p>
+projects.forEach((p) => {
+  container.innerHTML += `<div class="${p.show ? "box" : "box hide"}" title="${
+    p.description
+  }">
+  <a target="_blank" href=${p.url}>
+        <h2>${p.name}</h2></a><a target="_blank" href=${p.url}>
+        <img src=${getThumbnailUrl(p.name)} alt="${p.name}" /></a>
+        <p class="tags">${tagsToChips(p.tags)}</p>
         <div class="actions">
-        <a target="_blank" href=${projects[i].url}>
+        <a target="_blank" href=${p.url}>
         <i class="fas fa-eye"></i> View Live
         </a>
-        <a target="_blank" href=${projects[i].github}>
+        <a target="_blank" href=${p.github}>
         <i class="fab fa-github"></i> Github
         </a></div>
         </div>`;
-}
+});
 
 const hideBoxes = document.querySelectorAll(".hide");
 
