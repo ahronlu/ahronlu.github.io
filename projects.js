9b797526eeb803feacd914617b8b1a54cd329e5f
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-03-28 12:46:13 +0300

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,59 @@
+projects = [
+  {
+    name: "Proshop",
+    github: "https://github.com/ahronlu/proshop",
+    url: "https://koftovproshop.herokuapp.com/",
+    show: true,
+    description:
+      "eCommerce platform, built with Node.js, Express, React, MongoDB, Redux for state management and JWT for Authentication",
+    tags:
+      "#MongoDB #Express #React #Node #Redux #PWA #ReactReduxHooks #Bootstrap #JWT",
+  },
+  {
+    name: "Devconnector",
+    github: "https://github.com/ahronlu/devconnector",
+    url: "https://koftovdevconnector.herokuapp.com/",
+    show: true,
+    description:
+      "Social network for developers, built with Node.js, Express, React, MongoDB, Redux for state management and JWT for Authentication",
+    tags: "#MongoDB #Express #React #Node #Redux #Bootstrap #PWA #JWT",
+  },
+  {
+    name: "Instagram Clone",
+    github: "https://github.com/ahronlu/instagram",
+    url: "https://ahronluinstagram.herokuapp.com",
+    show: true,
+    description:
+      "Instagram clone, built with Node.js, Express, React, MongoDB and JWT for Authentication",
+    tags:
+      "#MongoDB #Express #Node #React #JWT #ContextAPI #ReactHooks #Formik #Bootstrap",
+  },
+  {
+    name: "Customer Manager",
+    github: "https://github.com/ahronlu/customer-manager",
+    url: "http://aharonlubitsch.herokuapp.com/",
+    show: true,
+    description:
+      "Customer manager app, built with Node.js, Express, React, MongoDB, Typescript and JWT for Authentication",
+    tags:
+      "#MongoDB #Express #Node #React #Typescript #ContextAPI #ReactHooks #SemanticUI #JWT",
+  },
+  {
+    name: "Devcamper API",
+    github: "https://github.com/ahronlu/devcamper-api",
+    url: "https://koftov-devcamper-api.herokuapp.com/",
+    show: true,
+    description:
+      "A real world backend for a bootcamp directory app using Node.js, Express, MongoDB and JWT for Authentication",
+    tags: "#MongoDB #Express #Node #JWT",
+  },
+  {
+    name: "React Social App",
+    github: "https://github.com/ahronlu/reactsocialfrontend",
+    url: "https://koftovsocial.netlify.app/",
+    show: true,
+    description:
+      "React social network app, built with React hooks , use-immer & Context API for state management",
+    tags: "#React #ContextAPI #Bootstrap",
+  }
+];
