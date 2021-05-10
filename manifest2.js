a78176fa85a7e839cf0f8bfb56d15d280a460569
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-10 22:10:40 +0300

    Create manifest2.js

---
+++ manifest2.js
@@ -0,0 +1,23 @@
+{
+  "name": "Random Cat Pics",
+  "short_name": "Random Cat Pics",
+  "orientation": "portrait-primary",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "white",
+  "theme_color": "black",
+  "icons": [
+    {
+      "src": "./cat192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "./cat.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
