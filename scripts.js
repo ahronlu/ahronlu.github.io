20606fd52ca7765b8b8144f1ef59f8d51143b24e
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-11 07:30:24 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const cards = document.querySelector(".cards");
 projects.forEach((p) => {
   cards.innerHTML += `
   <div class="card" title="${p.description}">
-      ${p.show && <a rel="noreferrer" target="_blank" href=${p.url}><img src=${getThumbnailUrl(p.name)} alt="${p.name}" /></a>}
+      ${p.show && '<a rel="noreferrer" target="_blank" href=${p.url}><img src=${getThumbnailUrl(p.name)} alt="${p.name}" /></a>'}
               <div class="card-info">
                 <h2>${p.name}</h2>
                 <p>${p.description}</p>
